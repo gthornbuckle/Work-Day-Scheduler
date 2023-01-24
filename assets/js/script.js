b36b00c15cc9b25fadf04c607433c84d9de4a9a2
gthornbuckle <gthornbuckle@gmail.com>
2023-01-24 16:48:51 +0000

    Changes the input field colour based on the current time

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,35 @@
 var currentDate = moment().format("[Today is: ]dddd, DDDo MMMM YYYY"); //Displays current date at the top of the page
 $("#currentDay").text(currentDate);
 
+var currentTime = moment().format("HH");
+
 displayHours = function(){ //Function to display working day hours
     var setHours = document.getElementsByTagName("h2"); //Takes all h2 elements from page
+    var setInputColor = document.getElementsByTagName("input");
     var currentHour = 9;
     var tempHour = 0;
+    var tempInput = 0;
 
     while (currentHour < 18){ //Loop to set hours 9:00 to 17:00
+
         setHours[tempHour].textContent = moment().hour(currentHour).format("HH:00"); //moment.js sets time
+        
+        if (currentTime == currentHour){
+            setInputColor[tempInput].classList.toggle("present");
+        }
+        else if (currentTime > currentHour){
+            setInputColor[tempInput].classList.toggle("past");
+        }
+        else {
+            setInputColor[tempInput].classList.toggle("future");
+        }
+
         tempHour++;
+        tempInput++;
         currentHour++;
     } 
 }
+
+console.log(currentTime);
 
 displayHours();
